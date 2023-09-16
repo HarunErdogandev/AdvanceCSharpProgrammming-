a867e54b72db20fff2ba9bdbea8853363d6f4266
Harun Erdoğan <harunerdgann21@gmail.com>
2023-09-16 22:42:11 +0300

    FactoryPattern

--- Advanced_C#_Programming/SchoolHRAdminstration/Program.cs
+++ Advanced_C#_Programming/SchoolHRAdminstration/Program.cs
@@ -1,28 +1,35 @@
 ﻿using HRAdminstrationAPI;
+using System.Numerics;
+using static SchoolHRAdminstration.Program;
 
 namespace SchoolHRAdminstration
 {
     internal class Program
     {
+       
         static void Main(string[] args)
         {
+
+            
             decimal totalSalaries = 0;
             List<IEmployee> employees = new List<IEmployee>();
 
-            SeedData(employees);
-
+            //SeedData(employees);
+            
             //foreach (var employee in employees)
             //{
             //    totalSalaries += employee.Salary;
             //}
             //Console.WriteLine($" Total Annual Salaries (including bonus):{totalSalaries}");
 
-            Console.WriteLine($" Total Annual Salaries (including bonuss):{employees.Sum(x=>x.Salary)}"); 
+            Console.WriteLine($" Total Annual Salaries (including bonuss):{employees.Sum(x=>x.Salary)}");
 
-
+            var result= EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher,1,"Harun","Edoğan",1200);
 
             Console.ReadKey();
         }
+      
+       
         public static void SeedData(List<IEmployee> employees)
         {
             IEmployee teacher1 = new Teacher
@@ -71,7 +78,10 @@ namespace SchoolHRAdminstration
 
         }
     }
-
+    public enum EmployeeType
+    {
+        Teacher, HeadOfDepartment, DebutyHeadMaster, HeadMaster
+    }
     public class Teacher : EmployeeBase
     {
         public override decimal Salary { get => base.Salary + (base.Salary * 0.02m); }
@@ -93,5 +103,38 @@ namespace SchoolHRAdminstration
     {
         public override decimal Salary { get => base.Salary + (base.Salary * 0.05m); }
 
+    }
+
+    public static class EmployeeFactory
+    {
+        public static IEmployee GetEmployee()
+        {
+            return new Teacher();
+        }
+        public static IEmployee GetEmployeeInstance(EmployeeType employeeType, int id
+            , string firstName, string lastName, decimal salary)
+        {
+            IEmployee? employee = null;
+            switch (employeeType)
+            {
+                case EmployeeType.Teacher:
+                    employee = FactoryPatern<IEmployee, Teacher>.GetInstance();
+                    break;
+                case EmployeeType.HeadOfDepartment:
+                    employee = new HeadOfDepartment { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+                    break;
+                case EmployeeType.DebutyHeadMaster:
+                    employee = new DebutyHeadMaster { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+
+                    break;
+                case EmployeeType.HeadMaster:
+                    employee = new HeadMaster { Id = id, FirstName = firstName, LastName = lastName, Salary = salary };
+
+                    break;
+                default:
+                    break;
+            }
+            return employee;
+        }
     }
 }
